3fa654bb129a8efa8602586c552f7efe6d6309e4
Nadezhda Limanova <dindinka86@gmail.com>
2023-10-06 15:53:48 +0300

    commit

--- cycles/calculator/views.py
+++ cycles/calculator/views.py
@@ -1,9 +1,7 @@
 import datetime
-from datetime import date, timedelta, datetime
-import os
+from datetime import timedelta, datetime
 
-from django.http import HttpResponse
-from django.shortcuts import render, reverse
+from django.shortcuts import render
 
 
 def process_input(request):
@@ -44,32 +42,3 @@ def process_input(request):
 
 
 
-
-# def home_view(request):
-#     template_name = 'app/home.html'
-#     pages = {
-#         'Главная страница': reverse('home'),
-#         'Показать текущее время': reverse('time'),
-#         'Показать содержимое рабочей директории': reverse('workdir'),
-#     }
-#
-#     # context и параметры render менять не нужно
-#     # подробнее о них мы поговорим на следующих лекциях
-#     context = {
-#         'pages': pages
-#     }
-#     return render(request, template_name, context)
-#
-#
-# def time_view(request):
-#     current_time = datetime.datetime.now()
-#     msg = f'Текущее время: {current_time}'
-#     return HttpResponse(msg)
-#
-#
-# def workdir_view(request):
-#     path = '.'
-#     result = sorted(os.listdir(path))
-#     msg = f'Список файлов в рабочей директории: {result}'
-#     return HttpResponse(msg)
-#     raise NotImplemented
